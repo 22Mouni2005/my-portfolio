76255c87a904e2657d174dcea52c9471d5f3b034
Mounika Karri <mounikaf6@gmail.com>
2025-09-10 14:26:47 +0530

    Update: name to Mounika Karri, profile image, metadata keywords, dev/start ports

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Mounika Karri - Full Stack Developer',
   description: '3rd-year B.Tech Student | Full Stack Web Developer | Passionate about coding and building modern web applications',
-  keywords: 'Mounika K, Full Stack Developer, Web Developer, B.Tech Student, React, JavaScript, Python, Java',
+  keywords: 'Mounika Karri, Full Stack Developer, Web Developer, B.Tech Student, React, JavaScript, Python, Java',
   authors: [{ name: 'Mounika Karri' }],
 }
 
